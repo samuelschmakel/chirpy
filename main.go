bb4a71e1941e86ef452c5964f094657a1c9e368e
SamSchmakel28 <samuel.schmakel@gmail.com>
2024-12-16 13:49:47 -0600

    Added metrics that check how many users have visited the app

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiconfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := apiconfig{}
 
 	fileSystem := http.Dir(filepathRoot)
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(fileSystem)))
-	mux.HandleFunc("/healthz", handler)
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(fileSystem))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -24,8 +33,15 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type:", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK) // status code is 200
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+func (cfg *apiconfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiconfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
